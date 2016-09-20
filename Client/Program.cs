4fea2406739dc962635e81d287999e88fff2f358
marco velasquez <marco.velasquez@outlook.com>
2016-09-20 18:02:15 -0500

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using ActorModel;
+using Akka.Actor;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var system = ActorSystem.Create("ClientSystem"))
+            {
+                var client = system.ActorOf(Props.Create<ProcessorActor>("ss"));
+
+                while (true)
+                {
+                    var input = Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
